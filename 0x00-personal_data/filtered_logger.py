0ac4f45f4ffe11f2f22ac214460c905f698d596c
badrxd <badrxdxd@gmail.com>
2024-02-07 22:16:05 +0100

    add if statment

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,9 @@ import re
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     '''function that returns the log message obfuscated'''
-    for e in fields:
-        pattern = r'{}=(.*?){}'.format(e, separator)
-        message = re.sub(
-            re.search(pattern, message).group(1), redaction, message)
-    return message
+    if fields and redaction and message and separator:
+        for e in fields:
+            pattern = r'{}=(.*?){}'.format(e, separator)
+            message = re.sub(
+                re.search(pattern, message).group(1), redaction, message)
+        return message
