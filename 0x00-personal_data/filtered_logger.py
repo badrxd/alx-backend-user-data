50c7ff44ce46151f5308906430bc44b77b76f4d1
badrxd <badrxdxd@gmail.com>
2024-02-08 03:28:16 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,6 +27,7 @@ class RedactingFormatter(logging.Formatter):
         self._fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        ''' formater'''
         msg = filter_datum(self._fields, self.REDACTION,
                            record.msg, self.SEPARATOR)
         record.msg = msg
