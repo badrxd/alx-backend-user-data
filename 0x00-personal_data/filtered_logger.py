4d12a8bec7de4dd9750c3b860aa4db778fa4af3e
badrxd <badrxdxd@gmail.com>
2024-02-08 03:42:09 +0100

    ee

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Regex-ing'''
-from typing import List, Match, Optional
+from typing import List
 import re
 import logging
 
@@ -22,9 +22,9 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self._fields = fields
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         ''' redact the message of LogRecord instance
