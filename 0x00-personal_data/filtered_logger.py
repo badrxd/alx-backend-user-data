0f617061d66001dd3c24b6d20e60561032690a8c
badrxd <badrxdxd@gmail.com>
2024-02-08 03:24:51 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 '''Regex-ing'''
 from typing import List, Match, Optional
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(e+'=(.*?)'+separator,
                          e+"="+redaction+separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self._fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg = filter_datum(self._fields, self.REDACTION,
+                           record.msg, self.SEPARATOR)
+        record.msg = msg
+        return super(RedactingFormatter, self).format(record)
