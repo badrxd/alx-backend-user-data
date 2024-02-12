11f921784ecbdc9a71dc76c020f06792e0f511ad
badrxd <badrxdxd@gmail.com>
2024-02-12 20:34:34 +0100

    task 3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python3
+'''authentication file'''
 from flask import request
 from typing import List,  TypeVar
-''''''
 
 
 class Auth():
+    '''class to manage the API authentication.'''
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''return False'''
         return False
 
     def authorization_header(self, request=None) -> str:
+        '''return None'''
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        '''return None'''
         return None
