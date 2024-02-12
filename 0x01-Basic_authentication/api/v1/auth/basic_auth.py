7a9ca631a349772b3936017b4dffd1d64fc943ec
badrxd <badrxdxd@gmail.com>
2024-02-12 21:51:09 +0100

    task 7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,7 @@ class BasicAuth(Auth):
         pass
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        ''''''
+        '''return the value after Basic : key'''
         if (not authorization_header or
                 type(authorization_header) is not str
                 or not authorization_header.startswith('Basic ')):
