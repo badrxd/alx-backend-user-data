3eb987694b76dc74a7717a2ad786f0bdbf910c70
badrxd <badrxdxd@gmail.com>
2024-02-12 22:19:02 +0100

    task 9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,7 +25,6 @@ class BasicAuth(Auth):
         '''function that returns the decoded value of a Base64 string'''
         if (not base64_authorization_header or
                 type(base64_authorization_header) is not str):
-            # or not base64_authorization_header.startswith('Basic ')):
             return None
         key = ''
         try:
@@ -33,3 +32,17 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return key.decode('utf-8')
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        '''function that returns the user email and
+        password from the Base64 decoded value'''
+        if (not decoded_base64_authorization_header or
+                type(decoded_base64_authorization_header) is not str):
+            return (None, None)
+        credentials = decoded_base64_authorization_header.split(":")
+
+        if len(credentials) != 2:
+            return (None, None)
+        return tuple(credentials)
