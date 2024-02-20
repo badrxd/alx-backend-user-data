78588b781f4e33156f45c7fb352ca62491d975f5
badrxd <badrxdxd@gmail.com>
2024-02-20 19:55:23 +0100

    task 15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,20 @@ def logout():
     return redirect("/")
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """ GET /sessions
+    Job:
+      - 
+    Return:
+      - 
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": f"{user.email}"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
