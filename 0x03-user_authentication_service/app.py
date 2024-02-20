262c7fa18f260a2daf3cbdaac7c5f21c52507099
badrxd <badrxdxd@gmail.com>
2024-02-20 19:02:34 +0100

    task 14. Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Route module for the project
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -49,6 +49,22 @@ def login():
     out = jsonify({"email": f"<{email}", "message": "logged in"})
     out.set_cookie("session_id", session_id)
     return out
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /sessions
+    Job:
+      - delete the session and redirect the user to the main page
+    Return:
+      - redirect
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(users.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
