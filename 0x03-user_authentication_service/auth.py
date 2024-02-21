71d710c50b7ca41bfcb36701b17cce844ed63902
badrxd <badrxdxd@gmail.com>
2024-02-21 10:53:39 +0100

    change password to hashed_password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,7 +103,8 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             password = _hash_password(password)
-            self._db.update_user(user.id, password=password, reset_token=None)
+            self._db.update_user(
+                user.id, hashed_password=password, reset_token=None)
             return None
         except NoResultFound:
             raise ValueError
