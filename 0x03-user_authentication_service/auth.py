8ef78c99b87d3f76ebf1fe3e5ff3d07ec7a16622
badrxd <badrxdxd@gmail.com>
2024-02-21 10:49:27 +0100

    task 18. Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,6 +104,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             password = _hash_password(password)
             self._db.update_user(user.id, password=password)
-            return reset_token
+            return None
         except NoResultFound:
             raise ValueError
