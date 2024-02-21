d5228566e0677c8f59a96ce6f99057576fbdda25
badrxd <badrxdxd@gmail.com>
2024-02-21 10:56:49 +0100

    change to kwargs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,7 +104,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             password = _hash_password(password)
             self._db.update_user(
-                user.id, hashed_password=password, reset_token=None)
+                user.id, **{'hashed_password': password, 'reset_token': None})
         except NoResultFound:
             raise ValueError
         return None
